fcbdba714f0653a5094f52f7ab286a4dd8da6535
Aden <cc31228@gmail.com>
2023-08-29 00:58:35 +0800

    update base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-week-2/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-week-3/' : '/',
   plugins: [react()],
 })
